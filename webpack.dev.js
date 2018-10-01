ef3fe78ede00bfefec0fe9acba31e5088dcd5fbf
Mischa MacLeod <mischa@mysinglesource.io>
2018-10-02 08:44:41 +1300

    Altered import/export structure to allow use in Webpack and NodeJS

---
+++ webpack.dev.js
@@ -0,0 +1,65 @@
+const webpack = require('webpack')
+const path = require('path')
+
+/*
+ * SplitChunksPlugin is enabled by default and replaced
+ * deprecated CommonsChunkPlugin. It automatically identifies modules which
+ * should be splitted of chunk by heuristics using module duplication count and
+ * module category (i. e. node_modules). And splits the chunks…
+ *
+ * It is safe to remove "splitChunks" from the generated configuration
+ * and was added as an educational example.
+ *
+ * https://webpack.js.org/plugins/split-chunks-plugin/
+ *
+ */
+
+/*
+ * We've enabled UglifyJSPlugin for you! This minifies your app
+ * in order to load faster and run less javascript.
+ *
+ * https://github.com/webpack-contrib/uglifyjs-webpack-plugin
+ *
+ */
+
+module.exports = {
+  module: {
+    rules: [{
+      include: [path.resolve(__dirname, 'src')],
+      loader: 'babel-loader',
+
+      options: {
+        presets: [ "@babel/preset-env" ]
+      },
+
+      test: /\.js$/
+    }]
+  },
+
+  entry: './lib/jose-core',
+
+  output: {
+    filename: 'jose.js',
+    library:'Jose',
+    libraryTarget: 'var',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  mode: 'development',
+
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          priority: -10,
+          test: /[\\/]node_modules[\\/]/
+        }
+      },
+
+      chunks: 'async',
+      minChunks: 1,
+      minSize: 30000,
+      name: true
+    }
+  }
+}
